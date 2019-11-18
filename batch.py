3874a8648bc4594ac070165ace68b5ed1919e648
mauro verzetti <mverzett@cern.ch>
2019-11-18 10:07:25 +0100

    way better file splitting

--- batch.py
+++ batch.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 import math
+from pdb import set_trace
 
 parser = ArgumentParser()
 parser.add_argument('f_in', help='file path')
@@ -10,8 +11,7 @@ parser.add_argument('--mc', action = 'store_true')
 args = parser.parse_args()
 
 all_files = [i.strip() for i in open(args.f_in) if i.strip() and not i.startswith('#')]
-chunk_size = int(math.ceil(len(all_files) / args.nchunks))
-infiles = all_files[chunk_size*args.ichunk:chunk_size*(args.ichunk+1)]
+infiles = [j for i, j in enumerate(all_files) if (i % args.nchunks) == args.ichunk]
 if infiles:
     print(len(infiles), "to be processed.")
 else:
